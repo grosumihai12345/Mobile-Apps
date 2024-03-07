7d80cb444e7aa4fa0b146bf41f6d943d2cef54c2
mihaitagonsoft <mihai.tagonsoft@gmail.com>
2024-03-07 17:12:13 +0200

    add assets_utils

---
+++ lib/utils/assets_utils.dart
@@ -0,0 +1,14 @@
+class AssetsUtils {
+  static String windIconPath = "assets/images/clouds/wind.png";
+  static String zapIconPath = "assets/images/clouds/zap.png";
+  static String windyRainySunIconPath =
+      "assets/images/clouds/windy_rainy_sun.png";
+  static String windyMoonIconPath = "assets/images/clouds/windy_rainy_sun.png";
+  static String rainySunIconPath = "assets/images/clouds/rainy_sun.png";
+  static String cloudyWindyMoonIconPath =
+      "assets/images/clouds/cloudy_windy_moon.png";
+  static String rainyMoonIconPath = "assets/images/clouds/rainy_moon.png";
+  static String bgCloudsImage = "assets/images/clouds/bg_clouds.png";
+  static String houseImage = "assets/images/house_image.png";
+  static String bgImage = "assets/images/background_image.png";
+}
