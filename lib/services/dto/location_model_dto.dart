350abc718dae38a2503af685e1787efc9e2d25e1
mihaitagonsoft <mihai.tagonsoft@gmail.com>
2024-03-20 12:40:29 +0200

    created location_model.dto

---
+++ lib/services/dto/location_model_dto.dart
@@ -0,0 +1,77 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'location_model_dto.g.dart';
+
+@JsonSerializable()
+class LocationResult {
+  final int id;
+  final String? name;
+  final double latitude;
+  final double longitude;
+  final double elevation;
+  @JsonKey(name: "feature_code")
+  final String featureCode;
+  @JsonKey(name: "country_code")
+  final String countryCode;
+  @JsonKey(name: "admin1_id")
+  final int? admin1Id;
+  @JsonKey(name: "admin2_id")
+  final int? admin2Id;
+  @JsonKey(name: "admin3_id")
+  final int? admin3Id;
+  @JsonKey(name: "admin4_id")
+  final int? admin4Id;
+  final String timezone;
+  final int population;
+  final List<String> postcodes;
+  @JsonKey(name: "country_id")
+  final int countryId;
+  final String? country;
+  final String? admin1;
+  final String? admin2;
+  final String? admin3;
+  final String? admin4;
+
+  LocationResult({
+    required this.id,
+    this.name,
+    required this.latitude,
+    required this.longitude,
+    required this.elevation,
+    required this.featureCode,
+    required this.countryCode,
+    this.admin1Id,
+    this.admin2Id,
+    this.admin3Id,
+    this.admin4Id,
+    required this.timezone,
+    required this.population,
+    required this.postcodes,
+    required this.countryId,
+    this.country,
+    this.admin1,
+    this.admin2,
+    this.admin3,
+    this.admin4,
+  });
+  factory LocationResult.fromJson(Map<String, dynamic> json) =>
+      _$LocationResultFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LocationResultToJson(this);
+}
+
+@JsonSerializable()
+class LocationResponse {
+  final List<LocationResult> results;
+  @JsonKey(name: "generationtime_ms")
+  final double generationtimeMs;
+
+  LocationResponse({
+    required this.results,
+    required this.generationtimeMs,
+  });
+  factory LocationResponse.fromJson(Map<String, dynamic> json) =>
+      _$LocationResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LocationResponseToJson(this);
+}
---
+++ lib/services/dto/location_model_dto.g.dart
@@ -0,0 +1,70 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'location_model_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+LocationResult _$LocationResultFromJson(Map<String, dynamic> json) =>
+    LocationResult(
+      id: json['id'] as int,
+      name: json['name'] as String?,
+      latitude: (json['latitude'] as num).toDouble(),
+      longitude: (json['longitude'] as num).toDouble(),
+      elevation: (json['elevation'] as num).toDouble(),
+      featureCode: json['feature_code'] as String,
+      countryCode: json['country_code'] as String,
+      admin1Id: json['admin1_id'] as int?,
+      admin2Id: json['admin2_id'] as int?,
+      admin3Id: json['admin3_id'] as int?,
+      admin4Id: json['admin4_id'] as int?,
+      timezone: json['timezone'] as String,
+      population: json['population'] as int,
+      postcodes:
+          (json['postcodes'] as List<dynamic>).map((e) => e as String).toList(),
+      countryId: json['country_id'] as int,
+      country: json['country'] as String?,
+      admin1: json['admin1'] as String?,
+      admin2: json['admin2'] as String?,
+      admin3: json['admin3'] as String?,
+      admin4: json['admin4'] as String?,
+    );
+
+Map<String, dynamic> _$LocationResultToJson(LocationResult instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'latitude': instance.latitude,
+      'longitude': instance.longitude,
+      'elevation': instance.elevation,
+      'feature_code': instance.featureCode,
+      'country_code': instance.countryCode,
+      'admin1_id': instance.admin1Id,
+      'admin2_id': instance.admin2Id,
+      'admin3_id': instance.admin3Id,
+      'admin4_id': instance.admin4Id,
+      'timezone': instance.timezone,
+      'population': instance.population,
+      'postcodes': instance.postcodes,
+      'country_id': instance.countryId,
+      'country': instance.country,
+      'admin1': instance.admin1,
+      'admin2': instance.admin2,
+      'admin3': instance.admin3,
+      'admin4': instance.admin4,
+    };
+
+LocationResponse _$LocationResponseFromJson(Map<String, dynamic> json) =>
+    LocationResponse(
+      results: (json['results'] as List<dynamic>)
+          .map((e) => LocationResult.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      generationtimeMs: (json['generationtime_ms'] as num).toDouble(),
+    );
+
+Map<String, dynamic> _$LocationResponseToJson(LocationResponse instance) =>
+    <String, dynamic>{
+      'results': instance.results,
+      'generationtime_ms': instance.generationtimeMs,
+    };
